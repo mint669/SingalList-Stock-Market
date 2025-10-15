e02c0479f355ec6def72123ae13d637191bfdbdc
mint669 <sda167744@gmail.com>
2025-10-16 02:19:06 +0700

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,10 @@
+type NavItem = {
+  href: string;
+  label: string;
+};
+
+export const NAV_ITEMS: NavItem[] = [
+  { href: "/", label: "Dashboard" },
+  { href: "/search", label: "Search" },
+  { href: "/watchlist", label: "Watchlist" },
+];
